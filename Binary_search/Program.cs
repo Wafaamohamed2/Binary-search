abfba95f07bd09e7e77e3a3ff44d3e4ed918e69c
Wafaamohamed2 <121331529+Wafaamohamed2@users.noreply.github.com>
2024-03-08 23:58:42 +0200

    Add files via upload
    Binary_search implementation in C#

---
+++ Binary_search/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_search
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] aray = { 5, 10, 15, 20, 25 };
+            Console.Write("Enter the value you look for : ");
+            int value = int.Parse(Console.ReadLine());
+            int len=aray.Length;
+            int check=Binary_search(aray, value,0,len-1);
+            if (check == -1) { Console.WriteLine("The value not founded "); }
+            else Console.WriteLine("The value is valied in position " + check);
+        }
+
+
+        static int Binary_search(int[] arr,int value, int l ,int h)
+        {
+            while (l<=h)
+
+            {
+                int mid = h+l / 2;
+                if (arr[mid] == value)
+                {
+                    return mid;
+
+                }
+               
+
+                    if (arr[mid] < value) { l = mid + 1; }
+
+                    else h = mid - 1;
+                
+                }
+                return -1;     
+            }
+          
+        }
+    }
+
